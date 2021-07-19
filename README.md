abea612a0f3d729c28e50c4917d9e1bd5747c6c6
Paulo Sandino <paulosandino@gmail.com>
2021-07-18 22:16:52 -0300

    removed useless files

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # java-knn
+
+Number of lines: 5095270
+
+## Run JCStress
+´´´
+java -jar java-knn-jcstress/target/jcstress.jar -r java-knn-jcstress/reports
+´´´
--- src/main/java/com/sandino/readers/CSVReader.java
+++
@@ -1,7 +0,0 @@
-package com.sandino.readers;
-
-import java.io.IOException;
-
-public interface CSVReader {
-    public String[] readLines() throws IOException;
-}
